6f2124c472c4a1d37fbd79775f74ec6539683843
ShivanandGadgi <101877101+ShivanandGadgi@users.noreply.github.com>
2022-03-19 17:41:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -23,14 +23,14 @@ $(document).ready(function(){
 	});
 
 	var typed = new Typed(".typing",{
-		strings: ["App Development","Freelancer","Web Development"],
+		strings: ["App Developer","Web Developer","Software Developer","SEO and ASO Expert","Freelancer"],
 		typeSpeed: 100,
 		backSpeed: 60,
 		loop: true
 	});
 
 	var typed = new Typed(".typing-2",{
-		strings: ["App Development","Freelancer","Web Development"],
+		strings: ["App Developer","Web Developer","Software Developer","SEO and ASO Expert","Freelancer"],
 		typeSpeed: 100,
 		backSpeed: 60,
 		loop: true
